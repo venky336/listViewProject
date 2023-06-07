9933f7fe1346ae87575d2352003a78931498e3ea
Venkateswara Rao Bankuru <bankuruvenkateswararao5@gmail.com>
2023-06-07 11:50:00 +0530

    finished project

---
+++ app/src/main/java/com/venky/listviewpractice/MainActivity.java
@@ -0,0 +1,30 @@
+package com.venky.listviewpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String listItems[]={"Apple","Banana","Rose","Apricot","Water melon"};
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_list_items,R.id.textView,listItems);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "item selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
